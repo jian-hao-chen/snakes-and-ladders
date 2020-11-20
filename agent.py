462ec72e22231c16a41cc6d742fa1b6d4f3a6386
jian-hao-chen <billchen85@gmail.com>
2020-11-20 23:56:10 +0800

    'v1' 完成，`README.md` 施工中

--- agent.py
+++ agent.py
@@ -12,31 +12,72 @@ import numpy as np
 class Agent(object):
     """Creates a virtual agent for interacting with the environment.
     """
-    def __init__(self, version, goal):
+    def __init__(self, version, goal, num_rand_dice, q_learn):
+        self.q_learn = q_learn
         if version == 'v0':
             # Generates a dictionary that map 0 ~ 5 to 1 ~ 6.
             self.action_space = dict((i, i + 1) for i in range(6))
             # The states start from 1 to goal
-            self.value_table = np.zeros(goal + 1)
-            self.eligibility = np.zeros(goal + 1)
         elif version == 'v1':
-            pass
+            # 2 fixed dice, [1, 1, 3, 3, 5, 5] and [2, 2, 4, 4, 6, 6]
+            if num_rand_dice == 0:
+                odd_die = {0: 1, 1: 1, 2: 3, 3: 3, 4: 5, 5: 5}
+                even_die = {0: 2, 1: 2, 2: 4, 3: 4, 4: 6, 5: 6}
+                self.action_space = [odd_die, even_die]
+            # Generates some random dice.
+            else:
+                self.action_space = []
+                for _ in range(num_rand_dice):
+                    self.action_space.append(
+                        dict((i, np.random.randint(6) + 1) for i in range(6)))
         elif version == 'v2':
             pass
         else:
             raise ValueError(f'Unknown argument: {version}.')
 
+        self.value_table = np.zeros(goal + 1)
+        self.eligibility = np.zeros(goal + 1)
+        if self.q_learn:
+            self.q_table = np.zeros((goal + 1, len(self.action_space)))
+        self.goal = goal
+
     def seek(self, state):
         acts = self.action_space
-        # Finds all probable states by action space
-        prob_states = np.array(list(acts.values())) + state
-        prob_states = prob_states[prob_states <= 100]
+        # For v0, a normal die.
+        if type(acts) == dict:
+            # Finds all probable states by action space
+            prob_states = np.array(list(acts.values())) + state
+            prob_states = prob_states[prob_states <= self.goal]
 
-        candidate = self.value_table[prob_states]
-        if candidate.all() == 0:
-            return self.sample()
-        else:
-            return acts[np.argmax(candidate)]
+            candidate = self.value_table[prob_states]
+            if candidate.all() == 0:
+                return self.sample()
+            else:
+                return acts[np.argmax(candidate)]
+        # For v1 or v2, multiple dice.
+        elif type(acts) == list:
+            n_choice = len(acts)
+            expectation = np.zeros(n_choice)
+            # Computes expectation of each choice.
+            for i in range(n_choice):
+                prob_states = np.array(list(acts[i].values())) + state
+                prob_states = prob_states[prob_states <= self.goal]
+                if len(prob_states) > 0:
+                    if self.q_learn:
+                        expectation[i] += (self.q_table[prob_states, i].sum() /
+                                           len(prob_states))
+                    else:
+                        expectation[i] += (
+                            self.value_table[prob_states].sum() /
+                            len(prob_states))
+                else:
+                    expectation[i] += 0
+            # Finds the action (dice) which has max expectation.
+            choice = np.argmax(expectation)
+            if expectation.all() == 0:
+                return self.sample()
+            else:
+                return acts[choice][np.random.randint(6)]
 
     def sample(self):
         # Single die sampling.
--- main.py
+++ main.py
@@ -29,12 +29,11 @@ parser.add_argument("-i",
                     default=0.5,
                     help=("the interval time of each round." +
                           " (default: 0.5 sec)"))
-parser.add_argument("-e",
+parser.add_argument("-E",
                     "--episode",
                     type=int,
                     default=10,
                     help="how many times to run.")
-
 parser.add_argument("-r",
                     "--random",
                     type=int,
@@ -44,6 +43,31 @@ parser.add_argument("-V",
                     "--visualize",
                     action="store_true",
                     help="set to display the training process.")
+parser.add_argument("-a",
+                    "--alpha",
+                    type=float,
+                    default=0.1,
+                    help="learning rate. (default: 0.1)")
+parser.add_argument("-y",
+                    "--gamma",
+                    type=float,
+                    default=0.9,
+                    help="decay rate. (default: 0.9)")
+parser.add_argument("-e",
+                    "--epsilon",
+                    type=float,
+                    default=0.1,
+                    help="epsilon used in `eps-greedy`. (default: 0.2)")
+parser.add_argument("-l",
+                    "--lambda",
+                    type=float,
+                    default=0.9,
+                    dest="Lambda",
+                    help="lambda used in `TD(lambda)`. (default: 0.9)")
+parser.add_argument("-q",
+                    "--q",
+                    action="store_true",
+                    help="Use Q-Learning as training policy.")
 args = parser.parse_args()
 VERSION = args.version
 SIZE = args.size
@@ -51,14 +75,23 @@ EPISODES = args.episode
 INTERVAL = args.interval
 NUM_RAND_DICE = args.random
 VISAULIZE = args.visualize
+ALPHA = args.alpha
+GAMMA = args.gamma
+EPSILON = args.epsilon
+LAMBDA = args.Lambda
+Q_LEARN = args.q
 
 
-def std_temp_diff(env, agent, alpha, gamma, Lambda):
+def std_temp_diff(env, agent):
+    history = np.zeros(EPISODES)
     for ep in range(EPISODES):
         last_state = 0
         while env.board.viewer.isopen:
-            # Chooses the action to take by checking value table.
-            action = agent.seek(last_state)
+            # Chooses the action with value table.
+            if np.random.rand() < EPSILON:
+                action = agent.sample()
+            else:
+                action = agent.seek(last_state)
             observation, reward, done, info = env.step(action)
             # If encounter 'ladder' or 'snake', info stored the state before
             # enter the aisle.
@@ -69,12 +102,12 @@ def std_temp_diff(env, agent, alpha, gamma, Lambda):
             # Updates eligibility by observation.
             agent.eligibility[cur_state] += 1
             # Computes TD error.
-            delta = (gamma * agent.value_table[cur_state] + reward -
+            delta = (GAMMA * agent.value_table[cur_state] + reward -
                      agent.value_table[last_state])
             # Updates the value table of agent.
-            agent.value_table += alpha * delta * agent.eligibility
+            agent.value_table += ALPHA * delta * agent.eligibility
             # Updates the eligibility of agent.
-            agent.eligibility *= gamma * Lambda
+            agent.eligibility *= GAMMA * LAMBDA
             # Updates last state.
             last_state = cur_state
 
@@ -83,24 +116,70 @@ def std_temp_diff(env, agent, alpha, gamma, Lambda):
             time.sleep(INTERVAL)
             if done:
                 print(f"Episode {ep+ 1} finished. Total steps: {env.steps}.")
+                history[ep] = env.steps
                 env.reset()
                 break
+    return history
 
 
 def q_learn(env, agent):
+    history = np.zeros(EPISODES)
     for ep in range(EPISODES):
         last_state = 0
         while env.board.viewer.isopen:
-            pass
+            # Chooses the action with value table.
+            if np.random.rand() < EPSILON:
+                action = agent.sample()
+            else:
+                action = agent.seek(last_state)
+            observation, reward, done, info = env.step(action)
+            # If encounter 'ladder' or 'snake', info stored the state before
+            # enter the aisle.
+            if info is not None:
+                cur_state = info
+            else:
+                cur_state = observation
+            # Computes TD error.
+            delta = (reward + GAMMA * agent.q_table[cur_state].max() -
+                     agent.q_table[last_state].max())
+            # Updates Q table.
+            agent.q_table[cur_state][
+                agent.q_table[cur_state].argmax()] += ALPHA * delta
+            # Updates last state.
+            last_state = cur_state
+
+            if VISAULIZE:
+                env.render(observation, info, agent.value_table[1:])
+            time.sleep(INTERVAL)
+            if done:
+                print(f"Episode {ep+ 1} finished. Total steps: {env.steps}.")
+                history[ep] = env.steps
+                env.reset()
+                break
+    return history
 
 
 if __name__ == "__main__":
     import numpy as np
+    import matplotlib.pyplot as plt
     # Initializes game environment and agent.
     game = SnakesAndLadders(VERSION, SIZE)
-    agent = Agent(VERSION, game.goal)
+    agent = Agent(VERSION, game.goal, NUM_RAND_DICE, Q_LEARN)
 
-    if VERSION == 'v0':
-        std_temp_diff(game, agent, 0.1, 0.9, 0.9)
+    if Q_LEARN:
+        history = q_learn(game, agent)
+    else:
+        history = std_temp_diff(game, agent)
+
+    fig = plt.figure(dpi=144)
+    ax = fig.add_subplot(1, 1, 1)
+    x = [x + 1 for x in range(EPISODES)]
+    ax.plot(x, history)
+    ax.set_ylim(0, 100)
+    ax.set_title('Steps per episode')
+    ax.set_xlabel('Episode')
+    ax.set_ylabel('Steps')
+    plt.savefig(f"img/{VERSION}_{SIZE}_{LAMBDA}.png")
+    plt.show()
 
     game.close()
